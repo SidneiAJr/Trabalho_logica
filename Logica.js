a2fdb833fa65360baf91abc89d3e3a0bea90e0df
SIDNEI ALBERTO KLEISCHMITT JUNIOR <03541674059@senacrs.edu.br>
2026-04-28 15:22:26 -0300

    Fix: Comentarios do codigo

--- Logica.js
+++ Logica.js
@@ -1,8 +1,19 @@
 /*
+O que é e esse codig?
+E um Jogo de Memoria feito com Hmtl css e JS Simples, onde o jogar clica com o mouse seleciona a carta e gera pontuação & conta as tentativas. 
 Explicação do Codigo:
-Primeiro um objeto cartas para pegar cada carta por sua classe.
-depois fazer uma seleção pela classe
-depois
+1 | Primeiro um objeto cartas para pegar cada carta por sua classe.
+2 | Depois fazer uma seleção pela classe
+3 | Depois Verifica o NodeList
+4 | Usamos forEach() para adicionar cada carta (agora embaralhada) ao contêiner container
+5 | Função que Vira as cartas & Embaralhas
+6 | Função que esconde as cartas
+7 | Função de Controle de destado
+8 | Função de Atualizar Pontuação
+9 | Função de Resetar pontuação
+10 | Função que esconde as cartas
+11 | Função Embaralha as cartas
+12 | Função Temporarizador 
 */
 
 const cartas = [
